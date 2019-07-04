f685495323c29d7a0aa2d9bea5f27a791f8f258e
Seunghwa <banksemi@naver.com>
2019-07-04 11:00:23 +0900

    Implement File Navigation

--- cloudike/src/app/drive/FileItem.ts
+++ cloudike/src/app/drive/FileItem.ts
@@ -6,13 +6,37 @@ export class FileItem
     public path: string;
     public bytes : number;
     public name: string;
+    public owner_path: string;
+    public mime_type: string;
+    public type: string;
+    public modified: string;
+    public role: string;
+    
     constructor(value)
     {
+        if (value == null) return null;
         this.author_name = value["author_name"];
         this.icon = value["icon"];
         this.path = value["path"];
         this.bytes = value["bytes"];
+        this.owner_path = value["owner_path"];
+        this.mime_type = value["mime_type"];
+        
+        this.modified = value["modified"];
+        this.role = value["role"];
 
+        this.type = this.icon;
+        if (this.type == "presentation_office") this.type = "Powerpoint"
+        if (this.type == "document_office") this.type = "Word"
+        if (this.type == "folder") 
+        {
+            if (this.role == "collaborator")
+                this.type = "공유 폴더";
+            else if (this.role == "owner")
+                this.type = "폴더";
+            else
+                this.type = "알 수 없음 .. 폴더";
+        }
         let temp = this.path.split("/");
         this.name = temp[temp.length - 1];
 
--- cloudike/src/app/drive/drive.component.ts
+++ cloudike/src/app/drive/drive.component.ts
@@ -2,33 +2,60 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UserInfo } from '../UserInfo';
 import { FileItem } from './FileItem';
+import { ViewEncapsulation } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+
 
 @Component({
   selector: 'app-drive',
   templateUrl: './drive.component.html',
-  styleUrls: ['./drive.component.css']
+  styleUrls: ['./drive.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DriveComponent implements OnInit {
 
-  public Root :FileItem;
-  public Now = [];
-  public Now_Path = "";
-  constructor(private http:HttpClient) { 
+  public static Root :FileItem;
+  public Now :FileItem = new FileItem(null);
+  public keepOriginalOrder = (a, b) => a.key;
+  public ParantFolder = [];
 
-    this.LoadFolder("");
+  constructor(private http:HttpClient, private router : Router) { 
+    router.events.subscribe( (event) => {
+
+      if (event instanceof NavigationEnd) {
+          // Show loading indicator
+          this.Update();
+      }
+  });
   }
 
   ngOnInit() {
     
   }
-  private LoadFolder(path: string)
-  {
-    this.http.get("https://api.cloudike.kr/api/1/metadata/?limit=500&offset=0&order_by=name",{
+  private Update()
+  {      
+    let url = this.router.url.substring("/drive".length);
+    if (url[0] == '/') url = url.substring(1);
+
+    this.http.get("https://api.cloudike.kr/api/1/metadata/" + url + "?limit=500&offset=0&order_by=name",{
       headers: {'Mountbit-Auth':UserInfo.token()}
-   }).subscribe(data => {
-      this.Root = new FileItem(data);
-      this.Now = Object.values(this.Root.content);
+    }).subscribe(data => {
+        DriveComponent.Root = new FileItem(data);
+        this.Now = new FileItem(data);
+        this.ParantFolder = [];
+        let path = "/drive";
+        this.ParantFolder.push({name:"내 Cloudike", path:path})
+
+        if (url.length > 0)
+        {
+          let temp = decodeURI(url).split('/');
+          temp.forEach(element => {
+            path += "/" + element;
+            this.ParantFolder.push({name:element, path:path})
+          });
+        }
     });
+
   }
 
 }
