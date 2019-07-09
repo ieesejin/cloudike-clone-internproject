5beb6f35d61d59080f3b4bb85fd9d7c97e546e3c
Seunghwa <banksemi@naver.com>
2019-07-09 15:17:17 +0900

    Fix that tree can't open automatically

--- cloudike/src/app/drive/nav-tree/folder-item/folder-item.component.ts
+++ cloudike/src/app/drive/nav-tree/folder-item/folder-item.component.ts
@@ -3,7 +3,7 @@ import { FileItem } from 'src/app/drive/FileItem';
 import { DriveComponent } from '../../drive.component';
 import { HttpClient } from '@angular/common/http';
 import { FileManagement } from '../../FileManagement';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-folder-item',
@@ -23,28 +23,38 @@ export class FolderItemComponent implements OnInit {
   }
 
   constructor(private http: HttpClient, private router: Router) { 
+    router.events.subscribe( (event) => {
+
+      if (event instanceof NavigationEnd) {
+          // Show loading indicator
+          this.Update();
+      }
+  });
   }
 
   ngOnInit() {
-    
+    this.Update();
+  }
+
+  private Update()
+  {
     // 이 아이템이 현재 경로에 있는 아이템일 경우 자동으로 클릭 효과
     let routerurl = decodeURI(this.router.url.substring("/drive".length));
     let folder = FileItem.SplitPath(routerurl);
     folder.forEach(element => {
       if (this.url == element.path)
       {
-        this.itemclick();
+        this.itemclick(true);
       }
     });
-
-
-
   }
-  public itemclick()
+
+  // value은 하위 목록을 열것인지, 열지 않을 것인지
+  public itemclick(value)
   {
     FileManagement.getItem(this.http, this.url, (item)=>{
       this.item = item;
-      item.content_hide = !item.content_hide;
+      item.content_hide = !value;
     });
   }
 }
