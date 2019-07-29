b1f7fa092c3b489a39bab65aa3855ab74fa96add
SejinLee <ieesejin@naver.com>
2019-07-29 14:51:31 +0900

    Fix thumbnails waiting icon problem

--- cloudike/src/app/drive/drive.component.ts
+++ cloudike/src/app/drive/drive.component.ts
@@ -199,16 +199,4 @@ export class DriveComponent implements OnInit {
         }
       });
   }
-
-  public getThumbnails(item : FileItem){
-    var smallThumbnails : string;
-
-    if(item.extraData['thumbnails'] != null){
-      if(item.extraData['thumbnails']['status'] == 'ready'){
-        smallThumbnails = item.extraData['thumbnails']['small']['link'];
-      }
-    }
-    return smallThumbnails;
-  }
-
 }
