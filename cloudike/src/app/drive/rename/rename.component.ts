3b5d7bce2fcfd957d481b1e1823505e4c165c3ec
damin0912 <32475781+damin0912@users.noreply.github.com>
2019-07-30 09:53:32 +0900

    Fix the file rename error
    fix the file name extension part / You can change the name without entering a file extension.

--- cloudike/src/app/drive/rename/rename.component.ts
+++ cloudike/src/app/drive/rename/rename.component.ts
@@ -12,8 +12,15 @@ import { HTTPService } from 'src/app/httpservice.service';
 })
 export class RenameComponent implements OnInit {
 
-  inputName: string = "";
   public selectitems = FileManagement.getSelectItemPath();
+  path : string = this.selectitems.toString();
+  b_name: string = this.path.replace("/","")
+      .replace(this.path.substr(this.path.lastIndexOf('.')),"");
+  
+  
+  //b_name is before name (unchanged name, except filename extension)
+  //b_name은 사용자가 이름을 변경하기 전의 이름, 확장자를 제외한다.
+
   constructor(private dialogRef: MatDialogRef<RenameComponent>, private router: Router, private hs: HTTPService) { }
 
   ngOnInit() {
@@ -23,15 +30,37 @@ export class RenameComponent implements OnInit {
   {
     this.selectitems.forEach(element => {
       var formdata = new FormData();
-      formdata.set("path", element);
-      formdata.set("newname", name);
 
-      console.log(element);
-      console.log(name);
-      this.hs.post("https://api.cloudike.kr/api/1/fileops/rename/", formdata, element + " 이름 변경").subscribe(data => {
-        // 성공
-      });
-  });
-    this.dialogRef.close();
+      if(element.indexOf( "." ) == -1){
+        //폴더의 이름 변경
+        formdata.set("path", element);
+        formdata.set("newname", name);
+
+        console.log(element);
+        console.log(name);
+        this.hs.post("https://api.cloudike.kr/api/1/fileops/rename/",
+        formdata, element + " 이름 변경").subscribe(data => { });
+      }
+  
+      else { //파일의 이름 변경
+        var ext = element.toString().substr(element.lastIndexOf('.') + 1);
+        //path에 있는 내용 중 .이후의 글자는 확장자
+        console.log("ext is "+ext);
+        ext = '.'+ext; //확장자에 '.'기호를 달아준다.
+
+        formdata.set("path", element);
+        formdata.set("newname", name+ext);
+        //api에 넘길 때, 자동으로 확장자를 붙여준다.
+        console.log(element);
+        console.log(name);
+        this.hs.post("https://api.cloudike.kr/api/1/fileops/rename/",
+        formdata, element + " 이름 변경").subscribe(data => { });
+      }
+      this.dialogRef.close();
+  
+    });
+
   }
+
+
 }
