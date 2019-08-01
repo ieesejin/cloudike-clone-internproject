1b0d8d1c84e2b0ea73b9d029d2b455aec75458db
Seunghwa <banksemi@naver.com>
2019-08-01 11:38:52 +0900

    Fix drop zone directive error and modify drop zone style
    need to npm update

--- cloudike/src/app/boot/boot.component.ts
+++ cloudike/src/app/boot/boot.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewRef, ElementRef } from '@angular/core';
 import { UserInfo } from '../UserInfo';
 
 import { Router, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { HTTPService } from '../httpservice.service';
+import { NgxDragAndDropDirective } from '@wkoza/ngx-upload/src/directives/dropzone.directive';
+import { FileItem } from '../drive/FileItem';
 @Component({
   selector: 'app-boot',
   templateUrl: './boot.component.html',
   styleUrls: ['./boot.component.css']
 })
 export class BootComponent implements OnInit {
+  @ViewChild('upload',{static: true}) public upload : ElementRef;
+  @ViewChild(NgxDragAndDropDirective,{static: true}) public ngx_upload;
+  public upload_class_list;
   constructor(private router: Router, private location: Location, private hs: HTTPService) { 
     // this.router.url is not working (because of no loading, BootComponent is working in bootstrap)
     // So use location class.
@@ -30,7 +35,21 @@ export class BootComponent implements OnInit {
     }
   }
 
+  get dragover()
+  {
+    if (this.upload_class_list.contains('dropZoneColorDrag'))
+      return true;
+
+    return new Date().getTime() - this.ngx_upload.last_dragover < 100;
+  }
+
+  get thisfolder()
+  {
+      var folder = FileItem.SplitPath(decodeURI(this.router.url.substring("/drive".length)));
+      return folder[folder.length - 1].name;
+  }
   ngOnInit() {
+    this.upload_class_list = this.upload.nativeElement.classList;
   }
   
 }
