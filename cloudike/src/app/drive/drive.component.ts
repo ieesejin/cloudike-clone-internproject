b74bd1894dc4696d0f2357f9015dff5168127a4e
Seunghwa <banksemi@naver.com>
2019-08-02 15:32:30 +0900

    Fix drag check box error

--- cloudike/src/app/drive/drive.component.ts
+++ cloudike/src/app/drive/drive.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, QueryList, ViewChild } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UserInfo } from '../UserInfo';
 import { FileItem } from './FileItem';
@@ -14,6 +14,7 @@ import { MoveFileComponent } from './move-file/move-file.component';
 import { HTTPService } from '../httpservice.service';
 import { ShareComponent } from './share/share.component';
 import { RenameComponent } from './rename/rename.component';
+import { SelectContainerComponent, SelectItemDirective } from 'ngx-drag-to-select';
 
 
 @Component({
@@ -25,11 +26,14 @@ import { RenameComponent } from './rename/rename.component';
 export class DriveComponent implements OnInit {
 
   @ViewChild('basicMenu', {static : true}) public basicMenu: ContextMenuComponent;
+  @ViewChild(SelectContainerComponent, {static : true}) selectContainer: SelectContainerComponent;
+  @ViewChildren(SelectItemDirective) SelectItemDirectives: QueryList<SelectItemDirective>;
+
   
   public static Root :FileItem = new FileItem(null);
   public static Now :FileItem = new FileItem(null);
   
-  public dragSelectItems;
+  public dragSelectItems : string[] = [];
 
   get nowfile():FileItem {
     return DriveComponent.Now;
@@ -70,10 +74,26 @@ export class DriveComponent implements OnInit {
 
   public onDragSelect(event)
   {
+    // 체크박스 리스트 다시 작성
     var element = document.getElementsByName("chk_info");
     element.forEach((inputbox : HTMLInputElement) => {
       inputbox.checked = this.dragSelectItems.includes(inputbox.value);
     });
+    
+    // 드래그 리스트 다시 작성
+    this.SelectItemDirectives.forEach(tabInstance =>
+      {
+        tabInstance.selected = this.dragSelectItems.includes(tabInstance.dtsSelectItem);
+    });
+
+    // 목록에 하나라도 있으면
+    if (this.nowfile.length != 0)
+    {
+      // 전체 체크박스 체크
+      var selectAllChkbox = <HTMLInputElement> document.getElementById("selectAllChkbox");
+      selectAllChkbox.checked = this.dragSelectItems.length == this.nowfile.length;
+    }
+
   }
   //전체선택 및 전체해제
   private selectAll(value){
@@ -85,22 +105,29 @@ export class DriveComponent implements OnInit {
     
     var selectAllChkbox = <HTMLInputElement> document.getElementById("selectAllChkbox");
     selectAllChkbox.checked = value;
+
+    if (value == true)
+    {
+      this.selectContainer.selectAll();
+    } else {
+      this.selectContainer.clearSelection();
+    }
     return result;
     
   }
 
   //각각의 체크박스를 다룰 때 
   private onCheckboxChange(item: FileItem, event) {
+    // 전체 체크박스 체크
     var selectAllChkbox = <HTMLInputElement> document.getElementById("selectAllChkbox");
-    //체크했을 때
-    if(event.target.checked) {
-      if (FileManagement.getSelectItemPath().length == Object.keys(this.nowfile.content).length)
-        selectAllChkbox.checked = true;
-    }
-    //체크를 풀었을 때
-    else {
-        selectAllChkbox.checked = false;
-    }
+    selectAllChkbox.checked = FileManagement.getSelectItemPath().length == this.nowfile.length;
+
+    // 체크한 목록을 드래그 상태에 포함
+    this.SelectItemDirectives.forEach(dragitem =>
+    {
+      if (dragitem.dtsSelectItem == item.path)
+        dragitem.selected = event.target.checked;
+    });
   }
 
   private getItemValue(list: CdkDropList, index: number)
@@ -175,11 +202,24 @@ export class DriveComponent implements OnInit {
 
   public CheckingItem(item)
   {
+    // 해당 아이템이 선택되있지 않으면
     var element = <HTMLInputElement>document.getElementById("chkbox" + item.name);
     if(element.checked == false)
     {
-      this.selectAll(false);
+      // 모든 아이템의 선택을 해제
+      var allelement = document.getElementsByName("chk_info");
+      allelement.forEach((element2 : HTMLInputElement) => {
+        element2.checked = false;
+      });
+
+      // 해당 아이템만 선택
       element.checked = true;
+
+      // 이를 기준으로 드래그 리스트 목록 다시 작성
+      this.SelectItemDirectives.forEach(tabInstance =>
+        {
+          tabInstance.selected = tabInstance.dtsSelectItem == item.path;
+        });
     }
   }
 }
